0da22d72eb49b4973e0191a7ddc5e3c3de413e08
Meheret17 <meheretalemu17@gmail.com>
2023-04-06 14:57:56 +0300

    organize files

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,18 @@
 //fizz buzz 
-for(let num=1;num<100;num++){
-    if(num%3==0 && num%5==0){
-        console.log(num+"=FizzBuzz")
+
+
+const FizzBuzz=(num)=>{
+    if(num%3===0 && num%5===0){
+        console.log(num+"=FizzBuzz");
     }
-    else if(num%3==0){
-        console.log(num+"=Fizz")
+    else if(num%3===0){
+        console.log(num+"=Fizz");
     }
-    else if(num%5==0){
-        console.log(num+"=Buzz")
+    else if(num%5===0){
+        console.log(num+"=Buzz");
     }
-    
-}
+    else
+    console.log("neither divisible by 3 nor 5");
+};
+
+FizzBuzz(45);
---
+++ loopT.js
@@ -0,0 +1,8 @@
+let size=8;
+for(let i=0;i<size;i++){
+    let line="";
+    for(let j=0;j<i;j++){
+        line+="#";
+    }
+    console.log(line);
+}
